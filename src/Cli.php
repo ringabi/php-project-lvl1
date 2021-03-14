9ad77373099cf421df1dfc1a6298bc6130abdf73
Rinat Gabidullin <gabidullin_rg@mail.ru>
2021-03-14 16:54:11 +0500

    fix bugs

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,9 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function greeting(): string
+function greeting()
 {
         line('Welcome to the Brain Games!');
-        $userName = prompt('May I have your name?', 'false', ' ');
+        $userName = prompt('May I have your name?', null, ' ');
         line("Hello, %s!", $userName);
 }
--- src/Engine.php
+++ src/Engine.php
@@ -5,10 +5,10 @@ namespace Brain\Games\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function runEngine(array $gameData, string $task): string
+function runEngine(array $gameData, string $task)
 {
     line('Welcome to the Brain Games!');
-    $userName = prompt('May I have your name?', 'false', ' ');
+    $userName = prompt('May I have your name?', null, ' ');
     line("Hello, %s!", $userName);
     print_r($task);
     foreach ($gameData as [$question, $answer]) {
@@ -16,7 +16,7 @@ function runEngine(array $gameData, string $task): string
             $userAnswer = prompt('Your answer');
         if ($answer !== $userAnswer) {
                 print_r("'$userAnswer' is wrong answer ;(. Correct answer was '$answer'.\n");
-                return print_r("Let's try again, $userName!\n");
+                print_r("Let's try again, $userName!\n");
         }
             print_r("Correct!\n");
     }
